831441d86de5c651db1c90a8a9dabeccbabab24d
Apex <mjh4117222@gmail.com>
2026-08-13 23:57:48 +0800

    fix: align Compose compiler to 1.5.14 for Kotlin 1.9.22

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.14" // 兼容 Kotlin 1.9.22
     }
 
     compileOptions {
--- core/ui-xiahong/build.gradle.kts
+++ core/ui-xiahong/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.14" // 兼容 Kotlin 1.9.22
     }
 
     compileOptions {
