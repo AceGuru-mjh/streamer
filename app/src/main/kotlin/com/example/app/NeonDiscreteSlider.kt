49cea0ced151a460a11909e54f5c439804917807
Apex <mjh4117222@gmail.com>
2026-08-14 14:15:58 +0800

    fix(app): add missing padding import in Neon slider preview

--- app/src/main/kotlin/com/example/app/NeonDiscreteSlider.kt
+++ app/src/main/kotlin/com/example/app/NeonDiscreteSlider.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
